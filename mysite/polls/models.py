f11843b61a50119cc22ad4a6ee36739141ccbeb6
Om-Alve <omalve8174@gmail.com>
2023-09-07 23:29:52 +0530

    Added more views

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 class Question(models.Model):
@@ -8,9 +8,9 @@ class Question(models.Model):
     
     def __str__(self):
         return self.question_text
-
-    def was_published_receny(self):
-        return self.pub_date >= timezone.now() 0 datetime.temedelata(dayes=1)
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     
 class Choice(models.Model):
     question = models.ForeignKey(Question,on_delete = models.CASCADE)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,Http404
+from .models import Question,Choice
+from django.template import loader
+
 
 def index(request):
-    return HttpResponse("Hello, world! You're at the polls index.")
+    latest_questions = Question.objects.order_by("-pub_date")[:5]
+    context = {
+            "latest_question_list": latest_questions,
+    }
+    return render(request,"polls/index.html",context)
+
+def detail(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,"polls/detail.html",{"question":question.question_text})
+def results(request,question_id):
+    return HttpResponse("You are looking at the results of question %s."% question_id)
+
+def vote(request,question_id):
+    return HttpResponse("You are voting for the question %s."% question_id)
+
 
 
